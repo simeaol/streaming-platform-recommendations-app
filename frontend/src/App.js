5d3ea97c58c3bdd4ec3aeab64ad7fd6d7d2e9091
nborba <nicholasborba@gmail.com>
2020-10-14 03:36:43 -0300

    More CSS, agora ta bonito

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,19 +7,19 @@ function App() {
   var [whishList, setList] = useState([])
   var [recommended, setRecommendation] = useState({})
 
-  function handleInclusion(){
-    const title = document.getElementById("search_field").value        
+  function handleInclusion() {
+    const title = document.getElementById("search_field").value
     console.log(title)
 
-    if(title){
+    if (title) {
       setList([...whishList, title])
-      document.getElementById("search_field").value=''
+      document.getElementById("search_field").value = ''
     }
     console.log(whishList)
 
   }
 
-  function handleExclusion(index){
+  function handleExclusion(index) {
     let elIndex = index;
     console.log(elIndex);
 
@@ -30,31 +30,31 @@ function App() {
     }
   }
 
-  async function getTitles (title) {
+  async function getTitles(title) {
     const { data } = await axios.get(`http://localhost:3333/?title=${title}`);
-   
 
-    console.log(`request result = ${ JSON.stringify(data)}`);
-    if(data){
+
+    console.log(`request result = ${JSON.stringify(data)}`);
+    if (data) {
       const items = data['items']
       console.log(items)
-      for(var index in items){
+      for (var index in items) {
         console.log(items[index])
       }
     }
     return data;
   }
 
-  async function getRecommendation(){
+  async function getRecommendation() {
     console.info(`Total of elements in whislist = ${whishList.length}`)
-    if(whishList.length === 0){      
+    if (whishList.length === 0) {
       alert('Favor, adicionar titulo de filmes ou series favoritas');
       return;
     }
     console.info(whishList);
     alert('Recomendacao');
-    console.log(whishList)   
-    
+    console.log(whishList)
+
     const response = await axios.post('http://localhost:3333/', {
       type: "",//TODO: monitization type includes: flatrate, ... if empty, all types will be considered!
       data: whishList,
@@ -62,24 +62,24 @@ function App() {
     const { provider } = response.data;
     const providerInfo = await axios.get(`http://localhost:3333/providers/${provider}`);
     console.log(`Recommended Provider=${providerInfo}`);
-    if(providerInfo.data){
+    if (providerInfo.data) {
       const { icon_url } = providerInfo.data;
       const id = icon_url.split('/')[2]
-      setRecommendation({name: providerInfo.data['clear_name'], image_url: `https://images.justwatch.com/icon/${id}/s100`});
+      setRecommendation({ name: providerInfo.data['clear_name'], image_url: `https://images.justwatch.com/icon/${id}/s100` });
     }
-    
+
   }
 
-  function renderRecommendation(){
-    if(recommended){
-      return(
-      <div>
-        <h5>Plataforma recomendado</h5>
-        <h6>{recommended.name}</h6>
-        <img src={recommended.image_url} alt=""/>
-      </div>
+  function renderRecommendation() {
+    if (recommended) {
+      return (
+        <div>
+          <h5>Plataforma recomendada</h5>
+          <h6>{recommended.name}</h6>
+          <img src={recommended.image_url} alt="" />
+        </div>
       )
-    }else{
+    } else {
       return <div></div>
     }
   }
@@ -91,24 +91,24 @@ function App() {
       </div>
       <div className="App" id="App_Title">
         <h1 className="app_title">Sistema de Recomendação</h1>
-        <span className="description">Netflix, Amazon Prime, Disney Plus, etc... Não sabe qual assinar? Deixa com a gente, digite os 
+        <span className="description">Netflix, Amazon Prime, Disney Plus, etc... Não sabe qual assinar? Deixa com a gente, digite os
           filmes e séries que você gosta e vamos te dizer qual a melhor plataforma pra você assinar ;)</span>
       </div>
       <div className="App" id="search_section">
-      <input type="text" id="search_field" className="search_input" placeholder="Digite o nome do filmes, series, etc..." />
+        <input type="text" id="search_field" className="search_input" placeholder="Digite o nome do filmes, series, etc..." />
         <button id="search_btn" onClick={handleInclusion}>Incluir</button>
       </div>
-      <div className="App">
+      <div className="App" id="my_movies_section">
         <ul className="list_of_my_movies">
           {whishList.map((name, index) => {
-            return <li key={ index }>{name} <button class="btn-delete" onClick={() => handleExclusion(index)}>X</button></li>;
+            return <li className="list_item" key={index}>{name} <button className="btn_remove" onClick={() => handleExclusion(index)}>X</button></li>;
           })}
         </ul>
       </div>
       <div className="App">
         <button id="recommendation_btn" onClick={getRecommendation}>Obter recomendação</button>
       </div>
-      <div className="App">    
+      <div className="App">
         {renderRecommendation()}
       </div>
       <div className="App-footer">
