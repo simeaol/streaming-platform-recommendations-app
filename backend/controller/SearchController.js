3fd7509bb721c547c7c559c0e52e79f3d0342c13
Simeao David Lamine <simeaolm@gmail.com>
2020-10-16 17:36:40 -0300

    Add back changed implementaion

--- backend/controller/SearchController.js
+++ backend/controller/SearchController.js
@@ -64,7 +64,7 @@ module.exports = {
             var providers = new Map();
             var justwatch = new JustWatch({locale:'pt_BR'});
             for(item of data){
-                let title = item['t'];
+                let title = item;
                 console.info(`Title item = ${title}`);
                 var result = await justwatch.search({query: title});
                 for(item of result['items']){                                       
